f579e7d242c75e1f19f58ac67f7566d7a54a7015
Hifjur Python <unknownhifjur@gmail.com>
2026-03-30 12:41:47 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
   const navLinks = [
     { name: "Home", path: "/" },
     { name: "Our Journey", path: "/anniversary" },
-    { name: "BirthDay", path: "/memories" },
+    { name: "BirthDay", path: "/birthday" }, // <-- changed from /memories to /birthday
     { name: "Diary", path: "/letter" },
     { name: "Special", path: "/future" },
     { name: "Photos", path: "/photos" },
