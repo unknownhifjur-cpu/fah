3c9d3a446c69086a7c8d524e51ef8c1cf56ec72b
Hifjur Python <unknownhifjur@gmail.com>
2026-03-30 12:35:45 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import Navbar from './components/Navbar'
 import Home from './pages/Home/Home'
-
 import Footer from './components/Footer'
 import { Route, Routes } from 'react-router-dom'
 import Anniversary from './pages/Anniversary/Anniversary'
-import Memories from './pages/Memories/Memories'
+import Birthday from './pages/Birthday/Birthday'  // new Birthday page
 import Letter from './pages/Letter/Letter'
 import Future from './pages/Future/Future'
 import NotFound from './pages/NotFound/NotFound'
@@ -17,15 +16,13 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/anniversary' element={<Anniversary/>} />
-        <Route path='/memories' element={<Memories/>} />
+        <Route path='/birthday' element={<Birthday/>} />   {/* new birthday route */}
         <Route path='/letter' element={<Letter/>} />
         <Route path='/future' element={<Future/>} />
         <Route path='/photos' element={<Photos/>} />
         <Route path='*' element={<NotFound/>} />
       </Routes>
       {/* <Footer/> */}
-      
-     
     </>
   )
 }
